1bc9236f28c44b1f6341fe4e15156a5a87e201d5
Riccardo Pasianotto <riccardo.pasianotto@gmail.com>
2019-09-27 11:43:34 +0200

    Definizione attori per coordinamento

---
+++ src/main/java/uniud/distribuiti/lastmile/car/TransportRequestMngr.java
@@ -0,0 +1,9 @@
+package uniud.distribuiti.lastmile.car;
+
+// Transport Request Manager acotr
+// Attore responsabile della gestione di una richiesta
+// ricevuta da un passeggero.
+// - valuta la disponibilita della macchiana a soddisfare la richiesta
+// - controlla distanza
+public class TransportRequestMngr {
+}
---
+++ src/main/java/uniud/distribuiti/lastmile/passenger/TransportRequest.java
@@ -0,0 +1,7 @@
+package uniud.distribuiti.lastmile.passenger;
+
+// TransportRequest actor
+// Questo è l'attore responsabile della gestione di una richiesta
+// di trasporto effettuata dal passeggero parent di questo attore
+public class TransportRequest {
+}
