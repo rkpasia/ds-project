d8a1e8aa28854b418191b9285fcb8a3e0282cea8
Riccardo Pasianotto <riccardo.pasianotto@gmail.com>
2019-10-02 15:54:00 +0200

    Definizione base classe TransitManager

---
+++ src/main/java/uniud/distribuiti/lastmile/car/TransitManager.java
@@ -0,0 +1,21 @@
+package uniud.distribuiti.lastmile.car;
+
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+import uniud.distribuiti.lastmile.location.TransportRoute;
+
+// Attore che gestisce il trasporto e lo spostamento
+// di una macchina all'interno della rete geografica
+public class TransitManager extends AbstractActor {
+
+    public static Props props(TransportRoute route){
+        return Props.create(TransitManager.class, () -> new TransitManager(route));
+    }
+
+    public TransitManager(TransportRoute route){}
+
+    @Override
+    public Receive createReceive(){
+        return receiveBuilder().build();
+    }
+}
